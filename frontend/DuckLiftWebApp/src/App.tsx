7685a9e3bc1546f6e07acc44859a937cb1ac9056
Netsbump <levasseur.sten@gmail.com>
2024-01-31 22:19:38 +0100

    chore: add storybooks, chakraUI

--- frontend/DuckLiftWebApp/src/App.tsx
+++ frontend/DuckLiftWebApp/src/App.tsx
@@ -1,10 +1,37 @@
 import "./App.css";
+import {
+  Button,
+  ChakraProvider,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 
 const App: React.FC = () => {
   return (
-    <>
-      <p>Hello world !</p>
-    </>
+    <ChakraProvider>
+      <FormControl isRequired>
+        <FormLabel>Adresse Email</FormLabel>
+        <Input type="email" />
+        <FormHelperText>We &#39;ll never share your email.</FormHelperText>
+      </FormControl>
+
+      <FormControl isRequired>
+        <FormLabel>Mot de passe</FormLabel>
+        <Input type="password" />
+      </FormControl>
+
+      <Button
+        isLoading
+        loadingText="Chargement"
+        colorScheme="teal"
+        variant="outline"
+        spinnerPlacement="start"
+      >
+        Se Connecter
+      </Button>
+    </ChakraProvider>
   );
 };
 
