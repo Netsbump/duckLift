78535bdba53d10324a4198cc3f9c419dd82c9db6
Netsbump <levasseur.sten@gmail.com>
2024-02-03 11:25:25 +0100

    chore : add global css in Chakra UI themes

--- frontend/DuckLiftWebApp/src/App.tsx
+++ frontend/DuckLiftWebApp/src/App.tsx
@@ -1,10 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
+import { theme } from "./themes/theme";
 
 export const App: React.FC = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   );
---
+++ frontend/DuckLiftWebApp/src/themes/theme.ts
@@ -0,0 +1,47 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      // Styles pour :root
+      ":root": {
+        "--chakra-colors-black": "#000", // exemple d'ajout de variable de couleur
+        "--chakra-colors-white": "#fff", // exemple d'ajout de variable de couleur
+        fontSynthesis: "none",
+        textRendering: "optimizeLegibility",
+        WebkitFontSmoothing: "antialiased",
+        MozOsxFontSmoothing: "grayscale",
+        fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
+        lineHeight: "1.5",
+        fontWeight: "400",
+        colorScheme: "light dark",
+        color: "#000",
+        backgroundColor: "#fff",
+      },
+      // Styles pour html
+      html: {
+        fontSize: "100%", // ou 16px, ou 1em
+      },
+      // Styles pour html et body
+      "html, body": {
+        margin: "0",
+        minHeight: "100vh",
+        width: "100%",
+      },
+      // Styles universels pour tous les éléments
+      "*, *::before, *::after": {
+        margin: "0",
+        padding: "0",
+        boxSizing: "border-box",
+      },
+      // Styles spécifiques pour #root
+      "#root": {
+        minHeight: "inherit",
+        width: "inherit",
+      },
+    },
+  },
+  // ... vous pouvez ajouter d'autres personnalisations de thème ici si nécessaire
+});
+
+export { theme };
