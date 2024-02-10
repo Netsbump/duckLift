f5338a62b6bbed3d8d8c40a3abbc6172b4a4f32e
Netsbump <levasseur.sten@gmail.com>
2024-02-10 01:35:11 +0100

    chore: add authentification context

--- frontend/DuckLiftWebApp/src/pages/Login.tsx
+++ frontend/DuckLiftWebApp/src/pages/Login.tsx
@@ -1,44 +1,69 @@
+import React, { useState } from "react";
 import {
   Button,
   FormControl,
-  FormHelperText,
   FormLabel,
   Input,
   VStack,
   HStack,
   Box,
 } from "@chakra-ui/react";
+import { useAuth } from "../contexts/Authentification";
+import { useNavigate } from "react-router-dom";
 
 export const Login: React.FC = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Simulation de l'authentification
+    login({ id: 1, name: "John Doe", email: email });
+    navigate("/");
+  };
+
   return (
-    <HStack
-      justifyContent={"space-around"}
-      minHeight={"inherit"}
-      alignItems={"center"}
-    >
-      <VStack>
-        <FormControl isRequired>
-          <FormLabel>Adresse Email</FormLabel>
-          <Input type="email" />
-          <FormHelperText>We &#39;ll never share your email.</FormHelperText>
-        </FormControl>
+    <form onSubmit={handleSubmit}>
+      {" "}
+      {/* Ajouter le gestionnaire de soumission ici */}
+      <HStack
+        justifyContent={"space-around"}
+        minHeight={"inherit"}
+        alignItems={"center"}
+      >
+        <VStack>
+          <FormControl isRequired>
+            <FormLabel>Adresse Email</FormLabel>
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormControl>
 
-        <FormControl isRequired>
-          <FormLabel>Mot de passe</FormLabel>
-          <Input type="password" />
-        </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Mot de passe</FormLabel>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormControl>
 
-        <Button
-          isLoading
-          loadingText="Chargement"
-          colorScheme="teal"
-          variant="outline"
-          spinnerPlacement="start"
-        >
-          Se Connecter
-        </Button>
-      </VStack>
-      <Box>Image</Box>
-    </HStack>
+          <Button
+            type="submit"
+            loadingText="Chargement"
+            colorScheme="teal"
+            variant="outline"
+            spinnerPlacement="start"
+          >
+            Se Connecter
+          </Button>
+        </VStack>
+        <Box>Image</Box>
+      </HStack>
+    </form>
   );
 };
--- frontend/DuckLiftWebApp/src/routes/RouteGuard.tsx
+++ frontend/DuckLiftWebApp/src/routes/RouteGuard.tsx
@@ -1,19 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/Authentification";
+import { useEffect } from "react";
 
 interface RouteGuardProps {
   children?: React.ReactNode;
-  requireAuth: boolean;
+  requireAuth?: boolean;
 }
 
 export const RouteGuard: React.FC<RouteGuardProps> = ({
   children,
-  requireAuth = true,
+  requireAuth = false,
 }) => {
+  const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
-  if (requireAuth) {
-    navigate("/login");
-  } else if (!requireAuth) {
-    navigate("/");
-  }
+
+  useEffect(() => {
+    if (requireAuth && !isAuthenticated) {
+      navigate("/login");
+    } else if (!requireAuth && isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated, requireAuth, navigate]);
+
   return <>{children}</>;
 };
