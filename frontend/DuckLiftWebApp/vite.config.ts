75b0046fef22ede80f314c7ceaa8c1154eaa493a
Netsbump <levasseur.sten@gmail.com>
2024-04-07 18:21:29 +0200

    chore : add docs folder for deploy

--- frontend/DuckLiftWebApp/vite.config.ts
+++ frontend/DuckLiftWebApp/vite.config.ts
@@ -8,6 +8,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "/duckLift/frontend/DuckLiftWebApp/",
+  build: {
+    outDir: "docs",
+  },
   test: {
     globals: true,
     environment: "jsdom",
