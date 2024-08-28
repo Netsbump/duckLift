ae024d82e36b989263b0da84031fe7134a55a5ec
unknown <levasseur.sten@gmail.com>
2024-08-28 23:28:59 +0200

    feat: Add PostgreSQL and pgAdmin setup for local development

---
+++ backend/api-ducklift/src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() user: User): Promise<User> {
+    return this.userService.create(user);
+  }
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+}
---
+++ backend/api-ducklift/src/user/user.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: ['ATHLETE', 'COACH', 'ADMIN'],
+    default: 'ATHLETE',
+  })
+  role: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
